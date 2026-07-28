97cbe5e8321f35bfd59f04cc53ea1c4fb5107cdc
Andrii Ariefiev <andrii_ariefiev@epam.com>
2026-07-28 07:03:37 +0200

    second commit

--- env/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket       = "mentoring-state-bucket"
-    key          = "terraformtask2/dev/terraform.tfstate"
-    region       = "us-east-1"
-    encrypt      = true
-    use_lockfile = true
-  }
-}
--- modules/rds/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "rds_instance_endpoint" {
-  value       = aws_db_instance.this.endpoint
-  description = "RDS endpoint (host:port)"
-}
-
-output "rds_instance_address" {
-  value       = aws_db_instance.this.address
-  description = "RDS hostname without port (use for MYSQL_HOST)"
-}
